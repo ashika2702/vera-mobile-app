9a5e85ed8db651f8a04f3184286aeb79c0297c3c
ashika2702 <ashika.m@stedaxis.com>
2026-06-27 11:21:20 +0530

    fixed

--- app/api/delivery/route/today/route.ts
+++ app/api/delivery/route/today/route.ts
@@ -1,4 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+export const fetchCache = 'force-no-store';
 import { query } from "../../../../../lib/db";
 import { getStartOfDayIST, formatDateToISO } from "../../../../../lib/timezone";
 import jwt from "jsonwebtoken";
